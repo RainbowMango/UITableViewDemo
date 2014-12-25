e197c58efb688be6e0779f7dc5b12db9fec04315
RainbowMango <qdurenhongcai@163.com>
2014-12-25 21:14:19 +0800

    first version finished

--- UITableViewDemo/UITableViewDemo/FoldTableViewController.swift
+++ UITableViewDemo/UITableViewDemo/FoldTableViewController.swift
@@ -16,16 +16,17 @@ class FoldTableViewController: UIViewController, UITableViewDataSource, UITableV
     let categoryNameLabelList = ["父01", "父02", "父03"]
     let categoryNameLabelSonList = ["子01", "子02", "子03"]
     
-    var singleCellDic: Dictionary<String, String>! = ["cellType": "Main", "isAttached": "false"]
-    var tableViewCells: Array<Dictionary<String, String>>!
+    var singleCellDic: Dictionary<String, String> = ["cellType": "Main", "isAttached": "false"]
+    var tableViewCells = Array<Dictionary<String, String>>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        println(self.singleCellDic)
+        tableViewCells.append(self.singleCellDic)
+        tableViewCells.append(self.singleCellDic)
+        tableViewCells.append(self.singleCellDic)
         setTableView()
         
-        tableViewCells.append(self.singleCellDic)
-        tableViewCells.append(self.singleCellDic)
-        tableViewCells.append(self.singleCellDic)
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,14 +47,12 @@ class FoldTableViewController: UIViewController, UITableViewDataSource, UITableV
         
         if self.tableViewCells[indexPath.row]["cellType"] == "Main" {
             let cell = tableView.dequeueReusableCellWithIdentifier("PrimeCategoryTableViewCell", forIndexPath: indexPath) as PrimeCategoryTableViewCell
-            //cell.categoryNameLabel.text = categoryNameLabelList[indexPath.row]
             cell.categoryNameLabel.text = "父cell"
             
             return cell
         }
         else {
             let cell = tableView.dequeueReusableCellWithIdentifier("MinorCategoryTableViewCell", forIndexPath: indexPath) as MinorCategoryTableViewCell
-            //cell.categoryNameLabel.text = categoryNameLabelSonList[indexPath.row]
             cell.categoryNameLabel.text = "子cell"
             return cell
         }
@@ -72,22 +71,39 @@ class FoldTableViewController: UIViewController, UITableViewDataSource, UITableV
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         self.tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
-        var path: NSIndexPath!
-        if self.tableViewCells[indexPath.row]["cellType"] == "Main" {
-            path = NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)
-        }
-        else {
-            path =  indexPath
-        }
-        
-        // 如果选中的cell类型
+        // 如果选中的cell类型为"Main"，则根据状态展开或收起菜单
         if self.tableViewCells[indexPath.row]["cellType"] == "Main" {
             //判断是否已经展开，是则收起，否则展开
             if self.tableViewCells[indexPath.row]["isAttached"] == "true" {
-                // 收起
+                // 改变状态
+                var item = self.tableViewCells[indexPath.row]
+                item.updateValue("false", forKey: "isAttached")
+                self.tableViewCells[indexPath.row] = item
+                
+                // 删除下拉菜单
+                self.tableViewCells.removeAtIndex(indexPath.row + 1)
+                
+                // 动态删除cell
+                var path = NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)
+                self.tableView.beginUpdates()
+                self.tableView.deleteRowsAtIndexPaths([path], withRowAnimation: UITableViewRowAnimation.Middle)
+                self.tableView.endUpdates()
             }
             else {
-                // 展开
+                // 改变状态
+                var item = self.tableViewCells[indexPath.row]
+                item.updateValue("true", forKey: "isAttached")
+                self.tableViewCells[indexPath.row] = item
+                
+                // 添加下拉菜单
+                var dic = ["cellType": "Sub", "isAttached": "false"]
+                self.tableViewCells.insert(dic, atIndex: indexPath.row + 1)
+                
+                // 动态增加cell
+                var path = NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)
+                self.tableView.beginUpdates()
+                self.tableView.insertRowsAtIndexPaths([path], withRowAnimation: UITableViewRowAnimation.Middle)
+                self.tableView.endUpdates()
             }
         }
         
